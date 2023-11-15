19957395fff3cfc317b89e322b60da30fd7f6e61
Berkay Kaya <80210343+imberkaykaya@users.noreply.github.com>
2023-11-15 18:23:46 +0300

    15 Kasım 23

--- GenericsIntro/Program.cs
+++ GenericsIntro/Program.cs
@@ -8,6 +8,17 @@ namespace GenericsIntro
 		{
 			MyList<string> isimler = new MyList<string>();
 			isimler.Add("Berkay");
-		}
+
+            Console.WriteLine(isimler.Length);
+
+			isimler.Add("Maho");
+
+            Console.WriteLine(isimler.Length);
+
+			foreach (var isim in isimler.Items)
+			{
+				Console.WriteLine(isim);
+            }
+        }
 	}
 }
